a1afc67ba7524af1391dca23b8ef2bd875595726
CloudDCrow <roki.robic88@gmail.com>
2023-02-09 17:14:18 +0100

    Made it so searching with either dex number or the name would return the image of that pokemon.
    Added a JSON file containing a list of the names of all pokemon, inputing the name of a pokemon looks for the index in the list and uses that index as the pokedex number
    
    +Small changes

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-(function() {
+(async function() {
   const submitBtn = document.getElementById("submit-btn");
   let pokemonNameInput = document.getElementById("pokemon-name");
   let pokemonImage = document.getElementById("pokemon-image");
+  let pokemonList = [];
 
   submitBtn.addEventListener("click", handleSubmitBtnClick);
   pokemonNameInput.addEventListener("keyup", handlePokemonNameInputKeyUp);
 
+  // Load the Pokemon list from the JSON file
+  if (pokemonList.length == 0) {
+    pokemonList = await (await fetch('pokemon_list.json')).json();
+  }
+
   function handleSubmitBtnClick() {
-    const pokemonName = pokemonNameInput.value;
+    let pokemonName = pokemonNameInput.value.toLowerCase().replace(/\s+|-|'|^0+/g, '');
+    let pokemonIndex = pokemonList.indexOf(pokemonName);
 
-    if(pokemonName > 0 & pokemonName < 1008) {
+    // If the Pokemon name is found in the list, set the image source
+    if (pokemonIndex !== -1) {
+      pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokemonIndex + 1}.png`;
+    } else if (pokemonName > 0 && pokemonName <= 1008) {
       pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokemonName}.png`;
-    }
-
-    else if(pokemonName == -69) {
-      pokemonImage.src = '69.png';
-    }
-
-    else {
-      pokemonImage.src = 'missi.png';
+      console.log(pokemonName);
+    } else {
+      pokemonImage.src = 'images/missi.png';
     }
   }
 
